aedeeb63d3b77866ce81553653f75500a78549fb
whz1045889164 <1045889164@qq.com>
2017-09-14 23:09:17 +0800

    首页布局完成，我的页布局一半

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,107 @@ import React from 'react'
 import {
     connect
 } from 'react-redux'
+import '../style/home.css'
+import "../iconfont/iconfont.css"
+import ReactSwipe from 'react-swipes'
+
+
 class HomeUI extends React.Component {
     componentDidMount() {
         // this.props.fetchListData();
     }
     render() {
         return (
-            <div>
-                <h1>首页</h1>
+            <div className="home">
+                <div className="logo"></div>
+                <ul className="nav">
+                    <li>
+                        <i className="icon iconfont icon-qiandao"></i>
+                        <p>签到</p>
+                    </li>
+                     <li>
+                        <i className="icon iconfont icon-yaoyiyao"></i>
+                        <p>摇一摇</p>
+                    </li>
+                     <li>
+                        <i className="icon iconfont icon-feiji1"></i>
+                        <p>全球购</p>
+                    </li>
+                     <li>
+                        <i className="icon iconfont icon-new"></i>
+                        <p>新品</p>
+                    </li>
+                </ul>
                 <ul>
+                <div className="seckill">
+                    <div className="title">
+                        <p>秒杀</p>
+                        <i className="icon iconfont icon-xiangyou1"></i>
+                    </div>
+                    <div className="seckillShop">
+                        <div className="seckillList">
+                            <img src="https://pic2.zhen.com/uploadimg1/300_c3b52036c720c5c4953de4e27ff866c0.jpg" />
+                            <p className="seckillName">Versace Jeans Couture</p>
+                            <p className="seckillPrice">￥339</p>
+                        </div>
+                        <div className="seckillList">
+                            <img src="https://pic2.zhen.com/uploadimg1/300_c3b52036c720c5c4953de4e27ff866c0.jpg" />
+                            <p className="seckillName">Versace Jeans Couture</p>
+                            <p className="seckillPrice">￥339</p>
+                        </div>
+                        <div className="seckillList">
+                            <img src="https://pic2.zhen.com/uploadimg1/300_c3b52036c720c5c4953de4e27ff866c0.jpg" />
+                            <p className="seckillName">Versace Jeans Couture</p>
+                            <p className="seckillPrice">￥339</p>
+                        </div>
+                        <div className="seckillList">
+                            <img src="https://pic2.zhen.com/uploadimg1/300_c3b52036c720c5c4953de4e27ff866c0.jpg" />
+                            <p className="seckillName">Versace Jeans Couture</p>
+                            <p className="seckillPrice">￥339</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="active">
+                    <img src="https://pic2.zhen.com/uploadimg1/c1969ee79a6c44544f278a738bd7f547.gif" alt=""/>
+                    <img src="https://pic2.zhen.com/uploadimg1/c1969ee79a6c44544f278a738bd7f547.gif" alt=""/>
+                </div>
+                <div className="shopList">
+                    
+                    <div className="shopInfo">
+                        <div className="show">
+                            <img src="https://pic2.zhen.com/uploadimg1/7bbe496f23c17f661084ccba339ab3af.jpg" alt=""/>
+                        </div>
+                        <div className="shopInfo2">
+                            <div className="shopInfolist">
+                                <img src="https://pic2.zhen.com/uploadimg1/255_140031ab496209dec03371d2862de5d2.jpg" alt=""/>
+                                <p className="shopName">Gucci</p>
+                                <p className="shopPrice">￥15399</p>
+                            </div>
+                            <div className="shopInfolist">
+                                <img src="https://pic2.zhen.com/uploadimg1/255_140031ab496209dec03371d2862de5d2.jpg" alt=""/>
+                                <p className="shopName">Gucci</p>
+                                <p className="shopPrice">￥15399</p>
+                            </div>
+                            <div className="shopInfolist">
+                                <img src="https://pic2.zhen.com/uploadimg1/255_140031ab496209dec03371d2862de5d2.jpg" alt=""/>
+                                <p className="shopName">Gucci</p>
+                                <p className="shopPrice">￥15399</p>
+                            </div>
+                            <div className="shopInfolist">
+                                <img src="https://pic2.zhen.com/uploadimg1/255_140031ab496209dec03371d2862de5d2.jpg" alt=""/>
+                                <p className="shopName">Gucci</p>
+                                <p className="shopPrice">￥15399</p>
+                            </div>
+                            <div className="shopInfolist">
+                                <img src="https://pic2.zhen.com/uploadimg1/255_140031ab496209dec03371d2862de5d2.jpg" alt=""/>
+                                <p className="shopName">Gucci</p>
+                                <p className="shopPrice">￥15399</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
                  {
         //     this.props.home_list.map((item, index)=>{
         //         return <li key={"h_l_" + index}>
--- src/components/My.js
+++ src/components/My.js
@@ -1,12 +1,85 @@
 import React from 'react'
-import {
-    Button
-} from 'antd';
+import '../style/my.css'
+import "../iconfont/iconfont.css"
+
 export default class My extends React.Component {
     render() {
         return (
-            <div>My
-                 <Button type="primary">Button</Button>
+            <div className="my">
+                <div className="top">
+                    <div className="set">
+                         <div>
+                        <i className="icon iconfont icon-shezhi"></i>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="headImg">
+                             <div>
+                            <img src="unlogin.png" alt=""/>
+                            </div>
+                        </div>
+                        <div className="login">
+                            <p>登录/注册</p>
+                        </div>
+                    </div>
+                    <div className="email">
+                    <div>
+                        <i className="icon iconfont icon-youjian1"></i>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="orders">
+                        <div className="all">
+                            <i className="icon iconfont icon-dingdan"></i>
+                            <p>全部订单</p>
+                        </div>
+                         <div className="waitmoney">
+                            <i className="icon iconfont icon-daifukuan"></i>
+                            <p>待付款</p>
+                        </div>
+                         <div className="waitgoods">
+                            <i className="icon iconfont icon-truck"></i>
+                            <p>待收货</p>
+                        </div>
+                         <div className="assess">
+                            <i className="icon iconfont icon-daipingjia"></i>
+                            <p>待评价</p>
+                        </div>
+                         <div className="change">
+                            <i className="icon iconfont icon-tuihuanhuo1"></i>
+                            <p>退换货</p>
+                        </div>
+                    </div>
+                     <div className="fortune">
+                        <div className="money">
+                            <i className="icon iconfont icon-dingdan"></i>
+                            <p>余额</p>
+                        </div>
+                         <div className="coupon">
+                            <i className="icon iconfont icon-daifukuan"></i>
+                            <p>优惠券</p>
+                        </div>
+                         <div className="integral">
+                            <i className="icon iconfont icon-truck"></i>
+                            <p>积分</p>
+                        </div>
+                         <div className="cards">
+                            <i className="icon iconfont icon-daipingjia"></i>
+                            <p>礼品卡</p>
+                        </div>
+                         <div className="income">
+                            <i className="icon iconfont icon-shouyi"></i>
+                            <p>收益</p>
+                        </div>
+                    </div>
+                            
+                    <div className="isMy">
+                        <div className="isTop">
+                        </div>
+                        <div className="isBottom"></div>
+                    </div>
+                </div>
             </div>
         )
     }
