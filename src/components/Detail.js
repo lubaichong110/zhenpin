fb8e4b224017380077436ee97ddde4a519b6dd61
yb935395192 <935395192@qq.com>
2017-09-20 14:51:22 +0800

    详情页拼接完成

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,117 +1,246 @@
 import React from 'react';
-import '../styles/detail.css';
+import '../styles/Detail.css';
 import {
     Carousel,
+    Tabs
 }
-
 from 'antd-mobile';
+const tabs = [{
+    title: "图文详情"
+}, {
+    title: "参数规格"
+}, ];
+
 class Detail extends React.Component {
     constructor() {
         super();
         this.state = {
-            arr: ["白", "黑", "黄", "红", "蓝", "从"]
+            arr: ["白", "黑", "黄", "红", "蓝", "从"],
+            good_id: "1001",
+            good_id_arr: [],
+            good_info: []
         }
     }
-    componentWillMount() {
-
+    componentDidMount() {
+        var that = this;
+        fetch("/api/getgooddata").then((res) => {
+            return res.json()
+        }).then((data) => {
+            for (var i = 0; i < data.length; i++) {
+                that.state.good_id_arr.push(data[i].goods_id);
+            }
+            var good_index = this.state.good_id_arr.indexOf(this.state.good_id);
+            this.state.good_info.push(data[good_index])
+            that.setState({
+                good_id_arr: this.state.good_id_arr,
+                good_info: this.state.good_info
+            })
+            console.log(data)
+        })
     }
-    selectedColor(index, e) {
+    selectedSize(index, e) {
         for (let i = 0; i < e.target.parentNode.children.length; i++) {
             e.target.parentNode.children[i].className = "choose_size_span"
         }
 
         e.target.className = "choose_size_span c_active"
     }
+    selectedColor(index, e) {
+        for (let i = 0; i < e.target.parentNode.children.length; i++) {
+            e.target.parentNode.children[i].className = "choose_color_span"
+        }
+
+        e.target.className = "choose_color_span c_active"
+    }
     render() {
         return (
-            <div id="detail">
-                <div className="good_scan">
-                    <div className="good_scan_pic">
-                       <Carousel
-                          className="my-carousel"
-                          autoplay={false}
-                          infinite
-                          selectedIndex={1}
-                          swipeSpeed={35}
-                          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-                          afterChange={index => console.log('slide to', index)}
-                        >
-                            <div className="img_div"><img src="https://pic2.zhen.com/uploadimg1/450_d8cf44550ee9609f6f1f970bdde7808b.JPG" alt=""/></div>
-                            <div className="img_div"><img src="https://pic2.zhen.com/uploadimg1/450_d8cf44550ee9609f6f1f970bdde7808b.JPG" alt=""/></div>
-                            <div className="img_div"><img src="https://pic2.zhen.com/uploadimg1/450_d8cf44550ee9609f6f1f970bdde7808b.JPG" alt=""/></div>
-                        </Carousel>
-                    </div>
-                    <div className="good_title_price">
-                        <div className="good_title">
-                            Michael Kors/麦克·科尔斯 不锈钢表壳精钢表带石英女表 MK5491 玫瑰金
+            <div>
+            {
+                this.state.good_info.map((item, index) => {
+                    return (
+                    <div key={item.goods_id} id="detail">
+                        <div className="good_scan">
+                            <div className="good_scan_pic">
+                               <Carousel
+                                  className="my-carousel"
+                                  autoplay={false}
+                                  infinite
+                                  selectedIndex={1}
+                                  swipeSpeed={35}
+                                  beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                                  afterChange={index => console.log('slide to', index)}
+                                >   
+                                {
+                                    item.goods_img.map((item_img,index) => {
+                                        return(
+                                             <div key={"a"+index} className="img_div"><img src={item_img} alt=""/></div>
+                                        )
+                                    })
+                                   
+                                }
+                                </Carousel>
+                            </div>
+                            <div className="good_title_price">
+                                <div className="good_title">
+                                    {item.goods_title}
+                                </div>
+                                <div className="good_price"><span className="rmb">¥</span>{item.goods_price}</div>
+                            </div>    
                         </div>
-                        <div className="good_price"><span className="rmb">¥</span>899</div>
-                    </div>    
-                </div>
-                <div className="good_choose">
-                    <div className="good_color">
-                        <span className="color_title">颜色</span>
-                        <ul className="choose_color">
-                            <li className="choose_color_span c_active">白色</li>
-                            <li className="choose_color_span">白色</li>
-                            <li className="choose_color_span">白色</li>
-                        </ul>
-                    </div>
-                    <div className="good_size">
-                        <span className="size_title">颜色</span>
-                        <ul className="choose_size">
-                        {
-                            this.state.arr.map((item,index) => {
-                                return(
-                                    <li key={"x" + index} 
-                                        onClick={(e) => this.selectedColor(index,e)} 
-                                        className="choose_size_span"
+                        <div className="good_choose">
+                            <div className="good_color">
+                                <span className="color_title">颜色</span>
+                                <ul className="choose_color">
+                                 {
+                                    item.goods_color.map((item_color,index) => {
+                                        return(
+                                            <li key={"b" + index} 
+                                                onClick={(e) => this.selectedColor(index,e)} 
+                                                className="choose_color_span"
 
-                                    >
-                                        {item}色
-                                    </li>
-                                )
-                            })
-                        }
-                        </ul>
-                    </div>
-                    <div className="good_send">
-                        <span className="send_title">配送</span>
-                        <ul className="send_ul">
-                            <li className="send_li">由珍品特约合作商发货</li>
-                        </ul>
-                    </div>
+                                            >
+                                                {item_color}色
+                                            </li>
+                                        )
+                                    })
+                                }
+                                </ul>
+                            </div>
+                            <div className="good_size">
+                                <span className="size_title">尺码</span>
+                                <ul className="choose_size">
+                                {
+                                    item.goods_size.map((item_size,index) => {
+                                        return(
+                                            <li key={"c" + index} 
+                                                onClick={(e) => this.selectedSize(index,e)} 
+                                                className="choose_size_span"
+
+                                            >
+                                                {item_size}
+                                            </li>
+                                        )
+                                    })
+                                }
+                                </ul>
+                            </div>
+                            <div className="good_send">
+                                <span className="send_title">配送</span>
+                                <ul className="send_ul">
+                                    <li className="send_li">由珍品特约合作商发货</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
                     
-                </div>
-            
-                <div className="good_promise">
-                    <section>
-                        <i className="iconfont icon-duihao"></i>
-                        正品保证
-                    </section>
-                    <section>
-                        <i className="iconfont icon-duihao"></i>
-                        正品保证
-                    </section>
-                </div>
-                <div className="brand_intro">
-                    <div className="brand_title">
-                        <span className="d_brand_name">品牌</span>
-                        <i className="iconfont icon-xiangyou"></i> 
-                    </div>
-                    <div className="d_brand_content">
-                        <div className="d_brand_pic">
-                            <img src="https://pic2.zhen.com/brand/1e453506b8f732fbafcc84f0ebe9bbe8.png" alt=""/>
+                        <div className="good_promise">
+                            <section>
+                                <i className="iconfont icon-duihao"></i>
+                                正品保证
+                            </section>
+                            <section>
+                                <i className="iconfont icon-duihao"></i>
+                                正品保证
+                            </section>
                         </div>
-                        <div className="brand_content_text">麦蔻 (McQ) 是亚历山大·麦昆 (Alexander McQueen) 的副线品牌，由创意总监莎拉•伯顿 (Sarah Burton) 掌舵，与主线品牌并驾齐驱，将当代个性设计与麦昆 (McQueen) 丰富的品牌历史完美结合，融合英国“街头风尚”和设计师诙谐睿智，男女成衣系列传递的精神是保持年轻的心和开放的态度。</div>
+                        <div className="brand_intro">
+                            <div className="brand_title">
+                                <span className="d_brand_name">品牌</span>
+                                <i className="iconfont icon-xiangyou"></i> 
+                            </div>
+                            <div className="d_brand_content">
+                                <div className="d_brand_pic">
+                                    <img src={item.goods_brand} alt=""/>
+                                </div>
+                                <div className="brand_content_text">麦蔻 (McQ) 是亚历山大·麦昆 (Alexander McQueen) 的副线品牌，由创意总监莎拉•伯顿 (Sarah Burton) 掌舵，与主线品牌并驾齐驱，将当代个性设计与麦昆 (McQueen) 丰富的品牌历史完美结合，融合英国“街头风尚”和设计师诙谐睿智，男女成衣系列传递的精神是保持年轻的心和开放的态度。</div>
+                            </div>
+                        </div>
+                        <div className="to_pic_detail">
+                                点击查看图文详情
+                                <i className = "iconfont icon-xiangyou" > </i>
+                        </div>
+                        <Tabs tabs={tabs}
+                                initialPage={0}
+                                onChange={(tab, index) => { console.log('onChange', index, tab); }}
+                                onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+                              >
+                                  <div className="childBox">
+                                      <div className="detail_content">
+                                      {
+                                          item.goods_img.map((item_detail_img,index)=>{
+                                            return(
+                                                <img key={"n" + index} src={item_detail_img}/>
+                                            )
+                                          })
+                                          
+                                      }
+                                      </div>
+                                  </div>
+                                  <div className="childBox childBox2" >
+                                      <div className="info_content">
+                                          <ul>
+                                            <li>
+                                              <span className="info_title">货号</span>
+                                              <span className="info_value">FY0551 K8F F0D9U</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">商品编码</span>
+                                              <span className="info_value">3115516</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">品牌</span>
+                                              <span className="info_value">Fendi</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">品牌属地</span>
+                                              <span className="info_value">意大利</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">水洗方式</span>
+                                              <span className="info_value">请依照水洗标签的提示进行洗涤</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">备注</span>
+                                              <span className="info_value">请按照保养说明进行保养</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">配件</span>
+                                              <span className="info_value">无</span>
+                                            </li>
+                                            <li>
+                                              <span className="info_title">材质</span>
+                                              <span className="info_value">100%棉</span>
+                                            </li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </Tabs> 
+                              <div className="d_footer">
+                                   <div className='d_collect'>
+                                        <div className="d_collection">
+                                            <i className="iconfont icon-shoucang"></i>
+                                            <span>收藏</span>
+                                        </div>
+                                   </div> 
+                                   <div className='d_cart'>
+                                        <div className="foot_cart">
+                                            <i className="iconfont icon-gouwudai"></i>
+                                            <span>购物袋</span>
+                                        </div>
+                                   </div>
+                                   <div className='add_cart'>                                       
+                                            加入购物袋
+                                   </div>  
+                                   <div className="buynow">
+                                            立即购买
+                                    </div>
+                              </div>
                     </div>
-                </div>
-                <div className="to_pic_detail">
-                    点击查看图文详情
-                    <i className="iconfont icon-xiangyou"></i>
-                </div>
-            </div>
+                    )
+                })
+            }
 
+            </div>
         )
     }
 }
